7706252b72f0ceec694314bd7f088462add8b9d4
Jordyn Marcellus <jordynmarcellus@gmail.com>
2019-12-08 08:50:31 -0500

    Creates new usePosition hook

---
+++ client/state/usePosition.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+
+const promisifiedGeoLocation = () =>
+  new Promise((resolve, reject) => {
+    return navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+
+// Based on Oleksii Trekhleb's implementation here: https://itnext.io/creating-react-useposition-hook-for-getting-browsers-geolocation-2f27fc1d96de
+
+export const usePosition = () => {
+  const [position, setPosition] = useState({
+    lat: null,
+    lng: null,
+  });
+  const [error, setError] = useState(null);
+
+  const onChange = ({ coords }) => {
+    setPosition({
+      lat: coords.latitude,
+      lng: coords.longitude,
+    });
+  };
+  const onError = error => {
+    console.log(error);
+    setError(error.message);
+  };
+  useEffect(() => {
+    async function getPos() {
+      const deviceGeoLocation = navigator.geolocation;
+      if (!deviceGeoLocation) {
+        return setError("Cannot access geolocation");
+      }
+      console.log("bomb's away");
+      await promisifiedGeoLocation()
+        .then(({ coords }) => onChange({ coords }))
+        .catch(error => onError(error));
+    }
+    getPos();
+  }, []);
+
+  return { ...position, error };
+};
